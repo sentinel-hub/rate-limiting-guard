8719a901e412ac7f1f214a54fd5aba9838f2f7a1
Anže Škerlavaj <anze.skerlavaj@sinergise.com>
2021-02-16 12:50:05 +0100

    Add rate limiting to mocksh service

---
+++ e2etest/mocksh/README.md
@@ -0,0 +1,14 @@
+# About mocksh
+
+This service is intended for internal use within the e2e tests. It provides a customizable replacement service for Sentinel Hub.
+
+It provides:
+- endpoints which are needed by `syncer` service (auth, policies and stats)
+- endpoints for changing the policies in effect
+- endpoint for filling the buckets (should be called about every second while the test is running)
+- endpoint for "fetching data" (which fails with response 429 if one of the buckets is empty)
+
+Note that when the test changes the policies, it should also send SIGHUP signal to `syncer` to trigger re-fetching of the policies from this mock service.
+
+Syncer service should be setup to use this service during tests (by `SENTINELHUB_ROOT_URL` env var) instead of Sentinel Hub.
+
--- e2etest/mocksh/mocksh.py
+++ e2etest/mocksh/mocksh.py
@@ -1,4 +1,7 @@
-from typing import List
+import asyncio
+import random
+import time
+from typing import List, Optional
 
 from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
@@ -44,6 +47,9 @@ app.state.POLICIES_RQ: List[Policy] = [
         "nanosBetweenRefills": 60000000,
     }
 ]
+app.state.buckets_pu = [p["capacity"] for p in app.state.POLICIES_PU]
+app.state.buckets_rq = [p["capacity"] for p in app.state.POLICIES_RQ]
+app.state.last_increment_time = time.monotonic()
 
 
 @app.post("/oauth/token")
@@ -80,13 +86,71 @@ def get_stats(request: Request):
 @app.put("/policies/PU")
 def put_policies_pu(policies: List[Policy], request: Request):
     request.app.state.POLICIES_PU = policies
+    # reset the rate limiting buckets values and the filling time:
+    request.app.state.buckets_pu = [p["capacity"] for p in policies]
+    request.app.state.last_increment_time = time.monotonic()
     return Response(status_code=202)
 
 
 @app.put("/policies/RQ")
 def put_policies_rq(policies: List[Policy], request: Request):
     request.app.state.POLICIES_RQ = policies
+    request.app.state.buckets_rq = [p["capacity"] for p in policies]
+    request.app.state.last_increment_time = time.monotonic()
     return Response(status_code=202)
+
+
+@app.post("/refill_buckets")
+async def post_refill_buckets(request: Request):
+    """
+    Maintenance endpoint - updates rate limiting buckets if needed. Should be called around every second or so.
+    """
+    now = time.monotonic()
+    time_passed = now - request.app.state.last_increment_time
+    if time_passed < 0.8:  # no need to refill too often
+        return
+    request.app.state.last_increment_time = now
+
+    # enough time has passed, we need to increment all buckets:
+    for i, policy in enumerate(request.app.state.POLICIES_RQ):
+        fill_per_s = 10 ** 9 / policy["nanosBetweenRefills"]
+        request.app.state.buckets_rq[i] = min(
+            policy["capacity"],
+            request.app.state.buckets_rq[i] + time_passed * fill_per_s,
+        )
+    for i, policy in enumerate(request.app.state.POLICIES_PU):
+        fill_per_s = 10 ** 9 / policy["nanosBetweenRefills"]
+        request.app.state.buckets_pu[i] = min(
+            policy["capacity"],
+            request.app.state.buckets_pu[i] + time_passed * fill_per_s,
+        )
+
+
+@app.get("/data")
+async def get_data(processing_units: int, request: Request):
+    """
+    This endpoint mocks a request for data on Sentinel Hub:
+    - checks buckets to determine if 429 should be returned
+    - decrements buckets
+    - sleeps for some time, then returns
+    """
+    # check if any policy is depleted - if so, return 429:
+    for bucket_value in request.app.state.buckets_rq:
+        if bucket_value < 1.0:
+            return Response(content="Rate limit reached", status_code=429)
+    for bucket_value in request.app.state.buckets_pu:
+        if bucket_value < processing_units:
+            return Response(content="Rate limit reached", status_code=429)
+
+    # otherwise decrement all of the buckets appropriately:
+    for i in range(len(request.app.state.buckets_rq)):
+        request.app.state.buckets_rq[i] -= 1.0
+    for i in range(len(request.app.state.buckets_pu)):
+        request.app.state.buckets_pu[i] -= processing_units
+
+    # sleep for some time and return response 200:
+    await asyncio.sleep(0.5 + random.random())  # 0.5 - 1.5 s delay
+    return Response(content="", status_code=200)
 
 
 if __name__ == "__main__":
